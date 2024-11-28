55d082648b1321d08414ed893871b272dd77877e
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2024-11-28 21:18:10 +0100

    Error pages were refuctored

--- src/main/java/com/softserve/itacademy/controller/advice/GlobalExceptionHandler.java
+++ src/main/java/com/softserve/itacademy/controller/advice/GlobalExceptionHandler.java
@@ -2,33 +2,43 @@ package com.softserve.itacademy.controller.advice;
 
 import com.softserve.itacademy.exception.EntityNotFoundException;
 import com.softserve.itacademy.exception.NullEntityReferenceException;
-import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(NullEntityReferenceException.class)
     public ModelAndView handleNullEntityReferenceException(NullEntityReferenceException e) {
-        ModelAndView modelAndView = new ModelAndView("500");
+        ModelAndView modelAndView = new ModelAndView("error");
         modelAndView.addObject("message", e.getMessage());
         return modelAndView;
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
     public ModelAndView handleEntityNotFoundException(EntityNotFoundException e) {
-        ModelAndView modelAndView = new ModelAndView("404");
+        ModelAndView modelAndView = new ModelAndView("error");
         modelAndView.addObject("message", e.getMessage());
         return modelAndView;
     }
 
-    @ExceptionHandler({RuntimeException.class})
-    public ModelAndView handleAnyOtherException(HttpServletResponse res, RuntimeException e) {
-        ModelAndView modelAndView = new ModelAndView("error");
-        modelAndView.addObject("code", res.getStatus());
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ModelAndView handleInternalServerErrorException(Exception e) {
+        ModelAndView modelAndView = new ModelAndView("500");
         modelAndView.addObject("message", e.getMessage());
+        return modelAndView;
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ModelAndView handleNotFoundException() {
+        ModelAndView modelAndView = new ModelAndView("404");
+        modelAndView.addObject("message", "The page you're looking for was not found.");
         return modelAndView;
     }
 
