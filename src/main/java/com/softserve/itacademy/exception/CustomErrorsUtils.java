e830f9901db380f9d00b346888f4576532ca37e7
Yurii Komarnytskyi <ykomarnytskyi2022@gmail.com>
2024-11-28 21:27:24 +0100

    Util class for validation and throwing custom erroers

---
+++ src/main/java/com/softserve/itacademy/exception/CustomErrorsUtils.java
@@ -0,0 +1,24 @@
+package com.softserve.itacademy.exception;
+
+import java.util.Objects;
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomErrorsUtils {
+	
+	Logger logger = LoggerFactory.getLogger(CustomErrorsUtils.class);
+	
+	public <T> void validateArgumentLogAndThrow(T argument, String exceptionMessage, String loggerMessage) throws NullEntityReferenceException, EntityNotFoundException {
+		if(Objects.isNull(argument) ) {
+			logger.error(loggerMessage);
+			throw new NullEntityReferenceException(exceptionMessage);
+		} else if((argument instanceof Optional<?> && ((Optional<?>) argument).isEmpty())) {
+			logger.error(loggerMessage);
+			throw new EntityNotFoundException(exceptionMessage);
+		}
+	}
+}
