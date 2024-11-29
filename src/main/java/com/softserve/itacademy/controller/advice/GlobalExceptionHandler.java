10a992c8fceebb65e69ff5c8da4efca2b2a3f59b
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2024-11-29 15:38:28 +0100

    Added logger

--- src/main/java/com/softserve/itacademy/controller/advice/GlobalExceptionHandler.java
+++ src/main/java/com/softserve/itacademy/controller/advice/GlobalExceptionHandler.java
@@ -3,6 +3,10 @@ package com.softserve.itacademy.controller.advice;
 
 import com.softserve.itacademy.exception.EntityNotFoundException;
 import com.softserve.itacademy.exception.NullEntityReferenceException;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,11 +14,15 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    public static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler(NullEntityReferenceException.class)
     public ModelAndView handleNullEntityReferenceException(NullEntityReferenceException e) {
+        logger.error("NullEntityReferenceException occurred: {}", e.getMessage());
         ModelAndView modelAndView = new ModelAndView("error");
         modelAndView.addObject("message", e.getMessage());
         return modelAndView;
@@ -22,6 +30,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(EntityNotFoundException.class)
     public ModelAndView handleEntityNotFoundException(EntityNotFoundException e) {
+        logger.warn("EntityNotFoundException occurred: {}", e.getMessage(), e);
         ModelAndView modelAndView = new ModelAndView("error");
         modelAndView.addObject("message", e.getMessage());
         return modelAndView;
@@ -30,6 +39,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ModelAndView handleInternalServerErrorException(Exception e) {
+        logger.error("Unhandled exception occurred: {}", e.getMessage(), e);
         ModelAndView modelAndView = new ModelAndView("500");
         modelAndView.addObject("message", e.getMessage());
         return modelAndView;
@@ -38,10 +48,9 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(NoHandlerFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ModelAndView handleNotFoundException() {
+        logger.info("NoHandlerFoundException: Page not found.");
         ModelAndView modelAndView = new ModelAndView("404");
         modelAndView.addObject("message", "The page you're looking for was not found.");
         return modelAndView;
     }
-
-
 }
